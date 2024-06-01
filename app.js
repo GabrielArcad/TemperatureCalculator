195cb52f7f9ac6e1bd569ea0ef9d8ba6076aac17
Arcadart <gabonoesgay@gmail.com>
2024-05-31 20:06:23 -0600

    File reorganization

--- app.js
+++
@@ -1,7 +0,0 @@
-function CalcTemp() {
-let Farenheit = document.getElementById("farenheit");
-let Result = (Number(Farenheit.value) -32)*(5/9);
-
-document.getElementById("Result").innerHTML = Result;
-}
-
