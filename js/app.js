f379fa70da18f0e9ab0a45add74447bb45cf8ff5
Arcadart <gabonoesgay@gmail.com>
2024-05-31 20:57:27 -0600

    Calculates temperature based on the item selected in dropdown

--- js/app.js
+++ js/app.js
@@ -1,14 +1,41 @@
+//Initialize the dropdown selected to Celsius
+function tempInit(){
+    document.getElementById("tempSelected").innerHTML = "Celsius";
+}
+
 //Change text of dropdown selected
 function showTemp(item){
+    //change the text in dropdown by the item selected
     document.getElementById("tempSelected").innerHTML = item.innerHTML;
+
+    
+    if(document.getElementById("tempSelected").innerHTML==='Celsius'){
+        document.getElementById("textResult").innerHTML = "Resultado en Celsius";
+        Result = 0;
+        document.getElementById("Result").innerHTML = Result;
+    }else if(document.getElementById("tempSelected").innerHTML==='Kelvin'){
+        document.getElementById("textResult").innerHTML = "Resultado en Kelvin";
+        Result = 0;
+        document.getElementById("Result").innerHTML = Result;
+    }
 }
 
 
-//Calulate temperature
+//Calulates temperature
 function CalcTemp() {
-let Farenheit = document.getElementById("farenheit");
-let Result = (Number(Farenheit.value) -32)*(5/9);
+    //Calculates temperature based on selection
+    if(document.getElementById("tempSelected").innerHTML==='Celsius'){
+        let tempValue = document.getElementById("tempValue");
+        let Result = (Number(tempValue.value) -32)*(5/9);
 
-document.getElementById("Result").innerHTML = Result;
+        document.getElementById("Result").innerHTML = Result;
+
+    }else if(document.getElementById("tempSelected").innerHTML==='Kelvin'){
+        let tempValue = document.getElementById("tempValue");
+        let Result = (Number(tempValue.value) -32)*(5/9)+273.15;
+
+        document.getElementById("Result").innerHTML = Result;
+    }
+
 }
 
