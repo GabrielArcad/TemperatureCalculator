476596512bf980a632a0cc761e79d7a4d9604be7
Arcadart <gabonoesgay@gmail.com>
2024-05-31 20:23:47 -0600

    Dropdown added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+//Change text of dropdown selected
+function showTemp(item){
+    document.getElementById("tempSelected").innerHTML = item.innerHTML;
+}
+
+
+//Calulate temperature
 function CalcTemp() {
 let Farenheit = document.getElementById("farenheit");
 let Result = (Number(Farenheit.value) -32)*(5/9);
